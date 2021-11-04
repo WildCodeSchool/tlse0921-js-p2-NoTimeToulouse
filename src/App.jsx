34a5c1b47b127b02cf737354bf1da6fe035094c7
F-Anais <f.anais31@gmail.com>
2021-11-04 16:30:36 +0100

    create contactform

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 import Card from './components/Card';
 import toulouseEvents from './events';
+import Form from './components/ContactForm';
 
 export default function App() {
   // eslint-disable-next-line no-unused-vars
   const [events, setEvents] = useState(toulouseEvents);
   return (
     <div>
-      {events.map((event) => <Card event={event} />)}
+      {events.map((event) => (
+        <Card event={event} />
+      ))}
+      <Form />
     </div>
-
   );
 }
---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+
+const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const resetForm = () => {
+    setName('');
+    setEmail('');
+    setMessage('');
+    setPhone('');
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    resetForm();
+  };
+
+  return (
+    <form onSubmit="handleSubmit">
+      <h2>Contactez-nous</h2>
+      <div className="form-content">
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={(e) => setName(e.target.value)}
+          placeholder="nom *"
+          value={name}
+          autoComplete="off"
+        />
+        <input
+          type="text"
+          id="phone"
+          name="phone"
+          onChange={(e) => setPhone(e.target.value)}
+          placeholder="téléphone"
+          value={phone}
+        />
+        <div className="email-content">
+          <input
+            type="mail"
+            id="email"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="email *"
+            value={email}
+            autoComplete="off"
+          />
+        </div>
+        <textarea
+          id="message"
+          name="message"
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="message *"
+          value={message}
+        />
+      </div>
+
+      <input
+        className="button"
+        type="button"
+        value="Envoyer"
+        onClick={handleSubmit}
+      />
+    </form>
+  );
+};
+export default ContactForm;

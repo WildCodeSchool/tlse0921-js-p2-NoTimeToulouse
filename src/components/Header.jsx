146c9e1e9bd3f471ca52182cbc7c2f14b0323915
KachiriMombrial <k.mombrial@gmail.com>
2021-11-07 17:27:01 +0100

    travail de style sur header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,35 @@
+import styled from 'styled-components';
+import logo from './assets/logo.png';
+
 const Header = () => (
-  <header>
-    <h1>No Time Toulouse</h1>
-    <img src="/assets/logo.png" alt="test" />
-    <ul>
-      <li>Évènements</li>
-      <li>Carte</li>
-      <li>Contact</li>
-      <li>Qui sommes-nous</li>
-    </ul>
-  </header>
+  <NavBar>
+    <div className="logo">
+      <img src={logo} alt="logo" />
+    </div>
+    <div className="navlist">
+      <ul>
+        <li className="item">
+          <a href="events">Évènements</a>
+        </li>
+        <li className="item">
+          <a href="cards">Carte</a>
+        </li>
+        <li className="item">
+          <a href="contact">Contact</a>
+        </li>
+        <li className="item">
+          <a href="whoarewe">Qui sommes-nous</a>
+        </li>
+      </ul>
+    </div>
+  </NavBar>
 );
+
+const NavBar = styled.menu`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  background-color: orange;
+`;
 
 export default Header;

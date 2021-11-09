c0f92b433ece509b3fe29c84fb05dc2b557aace1
cyril cabrolier <cyr.cab@hotmail.fr>
2021-11-09 15:57:27 +0100

    ajout de css

--- src/components/Mosaique.jsx
+++ src/components/Mosaique.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 // import { useState, useEffect } from 'react';
 // import axios from 'axios';
+import paintPicture from './Datas';
 
 const ImgContainer = styled.div`
   width: 80%;
@@ -13,6 +14,7 @@ const ImgContainer = styled.div`
 const LeftContainer = styled.div`
   width: 40%;
   height: 98%;
+  position: relative;
   background-color: lightgreen;
   transition: all 0.5s ease;
 
@@ -31,6 +33,7 @@ const RightContainer = styled.div`
 const SmallImg = styled.div`
   width: 49%;
   height: 48%;
+  position: relative;
   background-color: lightpink;
   transition: all 0.5s ease;
 
@@ -42,55 +45,83 @@ const Myimg = styled.img`
   width: 100%;
   height: 100%;
 `;
+const InfosImg = styled.div`
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  text-align: center;
+  margin: 0;
+  background-color: #fff;
+  opacity: 30%;
+`;
 
 const ImgGrid = () => (
-  //   const [pictures, setPictures] = useState([]);
+  // const [pictures, setPictures] = useState([]);
 
-  //   useEffect(() => {
-  //     axios
-  //       .get(
-  //         'https://api.unsplash.com/search/photos?query=peinture&client_id=cv-qpRxao8VaEyXIgyhc9nU4c7x38nuqEeVOjmDXlc0',
-  //       )
-  //       .then((response) => response.data)
-  //       .then((fetchedPictures) => setPictures(fetchedPictures.results));
-  //   }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get(
+  //       'https://api.unsplash.com/search/photos?query=peinture&client_id=cv-qpRxao8VaEyXIgyhc9nU4c7x38nuqEeVOjmDXlc0',
+  //     )
+  //     .then((response) => response.data)
+  //     .then((fetchedPictures) => setPictures(fetchedPictures.results));
+  // }, []);
 
-  //   const numberRand = Math.floor(Math.random() * pictures.length);
-  //   console.log(numberRand);
-  //   const imgRand = pictures[numberRand].urls.regular;
+  // const numberRand = Math.floor(Math.random() * 10);
+  // console.log(numberRand);
 
-  //   console.log(pictures);
   <ImgContainer>
     <LeftContainer>
       <Myimg
-        src="https://images.unsplash.com/photo-1557401018-aa08a2ddf43c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cGVpbnR1cmV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+        // src={pictures[2].urls.regular}
+        src={paintPicture.urls.regular}
         alt="test"
+        links={paintPicture.links}
       />
+      <InfosImg>
+        <p>Photo by {paintPicture.user.username} on Unsplash</p>
+      </InfosImg>
     </LeftContainer>
     <RightContainer>
       <SmallImg>
         <Myimg
-          src="https://images.unsplash.com/photo-1549810950-43ff06f843f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHBlaW50dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+          // src={pictures[6].urls.regular}
+          src={paintPicture.urls.regular}
           alt="image_test"
         />
+        <InfosImg>
+          <p>Photo by {paintPicture.user.username} on Unsplash</p>
+        </InfosImg>
       </SmallImg>
       <SmallImg>
         <Myimg
-          src="https://images.unsplash.com/photo-1599334682702-dcb76119754c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cGVpbnR1cmV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+          // src={pictures[6].urls.regular}
+          src={paintPicture.urls.regular}
           alt="image_test"
         />
+        <InfosImg>
+          <p>Photo by {paintPicture.user.username} on Unsplash</p>
+        </InfosImg>
       </SmallImg>
       <SmallImg>
         <Myimg
-          src="https://images.unsplash.com/photo-1555885489-5381a21766ed?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHBlaW50dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+          // src={pictures[6].urls.regular}
+          src={paintPicture.urls.regular}
           alt="image_test"
         />
+        <InfosImg>
+          <p>Photo by {paintPicture.user.username} on Unsplash</p>
+        </InfosImg>
       </SmallImg>
       <SmallImg>
         <Myimg
-          src="https://images.unsplash.com/photo-1618335321517-d8713bdbc418?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHBlaW50dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+          // src={pictures[6].urls.regular}
+          src={paintPicture.urls.regular}
           alt="image_test"
         />
+        <InfosImg>
+          <p>Photo by {paintPicture.user.username} on Unsplash</p>
+        </InfosImg>
       </SmallImg>
     </RightContainer>
   </ImgContainer>

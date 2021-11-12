93383d24e2e1a5179e75549935a2ee955f9e3872
cyril cabrolier <cyr.cab@hotmail.fr>
2021-11-12 20:12:26 +0100

    contour cards events

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,34 +1,54 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const EventsCard = styled.div`
+const ContainerCards = styled.div`
   margin: auto;
   display: flex;
-  width: 90%;
+  width: 100%;
   flex-direction: column;
   justify-content: space-between;
+  font-size: 12px;
+`;
+const EventCard = styled.div`
+  border: 1px solid #f06e96;
+  padding: 2em;
 `;
 const ListInfoCard = styled.ul`
   list-style-type: none;
   padding: 0;
 `;
+const CardsTitle = styled.h2`
+  font-size: 1.2em;
+`;
+const ButtonOpenEvent = styled.button`
+  padding: 0.5em;
+  height: 50%;
+`;
+const CardFooter = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 const Card = ({ eventsToDisplay }) => (
-  <EventsCard>
+  <ContainerCards>
     {eventsToDisplay.map((event) => (
-      <div>
-        <h2>{event.fields.name}</h2>
+      <EventCard>
+        <CardsTitle>{event.fields.name}</CardsTitle>
         <div className="description">
           <p>{event.fields.littleDescription}</p>
         </div>
-        <ListInfoCard className="infospratiques">
-          {event.fields.dates && <li>{event.fields.dates}</li>}
-          {event.fields.city && <li>{event.fields.city}</li>}
-          {event.fields.eventTheme && <li>{event.fields.eventTheme}</li>}
-          {event.fields.booking && <li>{event.fields.booking}</li>}
-        </ListInfoCard>
-      </div>
+        <CardFooter>
+          <ListInfoCard className="infospratiques">
+            {event.fields.dates && <li>{event.fields.dates}</li>}
+            {event.fields.city && <li>{event.fields.city}</li>}
+            {event.fields.eventTheme && <li>{event.fields.eventTheme}</li>}
+            {event.fields.price && <li>{event.fields.price}</li>}
+          </ListInfoCard>
+          <ButtonOpenEvent>Voir plus</ButtonOpenEvent>
+        </CardFooter>
+      </EventCard>
     ))}
-  </EventsCard>
+  </ContainerCards>
 );
 export default Card;

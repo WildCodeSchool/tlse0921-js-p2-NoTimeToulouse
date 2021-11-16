17121b32eb96d4f7ed2e0ff51e59a5c666dbbed3
cyril cabrolier <cyr.cab@hotmail.fr>
2021-11-16 17:31:04 +0100

    ajout d'un hook perso et début de l'affichage en ligne

---
+++ src/components/pages/accueil/EventsInline/DisplayEventsInline.jsx
@@ -0,0 +1,5 @@
+// import useFetchTheme from "./useFetchTheme";
+
+// const EventList = () => {
+//     const {events, isLoading, erros} = useFetchTheme('https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=agenda-des-manifestations-culturelles-so-toulouse&q=&rows=200&facet=date_debut&facet=date_fin&facet=categorie_de_la_manifestation&facet=theme_de_la_manifestation&refine.theme_de_la_manifestation=Musique+classique');
+// }
---
+++ src/components/pages/accueil/EventsInline/useFetchTheme.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+const useFetchTheme = (url) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        setIsLoading(false);
+        return res;
+      })
+      .then(setData)
+      .catch((err) => {
+        setError(true);
+        console.log(err);
+      });
+  }, []);
+
+  return { data, error, isLoading };
+};
+
+export default useFetchTheme;

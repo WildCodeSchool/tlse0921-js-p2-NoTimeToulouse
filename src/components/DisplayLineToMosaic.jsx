ece7e7df8e296ee2f61c210d5764a9c99b49e4fc
Emma <emma.nouti@gmail.com>
2021-11-16 15:52:45 +0100

    ajout css sur bouton

--- src/components/DisplayLineToMosaic.jsx
+++ src/components/DisplayLineToMosaic.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const MosaicButton = (props) => {
   const { toMosaic, handleDisplayClick } = props;
   return (
-    <div>
-      <button type="button" onClick={handleDisplayClick}>
+    <MosaicButtonContainer>
+      <LineToMosaicButton type="button" onClick={handleDisplayClick}>
         Affichage {toMosaic ? 'en ligne' : 'en mosaïque'}
-      </button>
-    </div>
+      </LineToMosaicButton>
+    </MosaicButtonContainer>
   );
 };
 
 export default MosaicButton;
+
+const LineToMosaicButton = styled.input`
+  width: 20%;
+  height: 50px;
+  color: var(--important-color);
+  font-weight: bold;
+  border-radius: 5px;
+  border: white solid 3px;
+  background-color: white;
+`;
+const MosaicButtonContainer = styled.div`
+  width: 100%;
+  margin-top: 1em;
+  display: flex;
+  align-items: flex-start;
+`;
